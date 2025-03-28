d43c059f6ee29dc8ddc5f8546105a23fe696e67c
WesleyBurgt <wesleyburgt@gmail.com>
2025-03-28 11:18:59 +0100

    Added speed scaling to hud
    also made cinemachine follow show the slower speed better

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -6,6 +6,7 @@ public class HUD : MonoBehaviour
     [SerializeField] private TMP_Text _fuelText;
     [SerializeField] private TMP_Text _damageText;
     [SerializeField] private CarControl _carController;
+    [SerializeField] private float _speedScale = 1;
 
     void Start()
     {
@@ -14,7 +15,7 @@ public class HUD : MonoBehaviour
 
     private void LateUpdate()
     {
-        _speedText.text = Mathf.RoundToInt(_carController.CurrentSpeed).ToString() + "kmph";
+        _speedText.text = Mathf.RoundToInt(_carController.CurrentSpeed * _speedScale).ToString() + "kmph";
         _fuelText.text = Mathf.RoundToInt(_carController.Fuel).ToString() + "L";
         _damageText.text = Mathf.RoundToInt(_carController.damagePercentage).ToString() + "% dmg";
     }
