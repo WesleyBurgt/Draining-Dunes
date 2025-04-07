48ed1b4f2bc10e480a75e8be0cafb2cc7e7f656f
WesleyBurgt <wesleyburgt@gmail.com>
2025-04-07 10:18:51 +0200

    Added money into HUD

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,8 +5,10 @@ public class HUD : MonoBehaviour
     [SerializeField] private TMP_Text _speedText;
     [SerializeField] private TMP_Text _fuelText;
     [SerializeField] private TMP_Text _damageText;
+    [SerializeField] private TMP_Text _moneyText;
     [SerializeField] private CarControl _carController;
     [SerializeField] private float _speedScale = 1;
+    [SerializeField] public int _money;
 
     void Start()
     {
@@ -18,5 +20,6 @@ public class HUD : MonoBehaviour
         _speedText.text = Mathf.RoundToInt(_carController.CurrentSpeed * _speedScale).ToString() + "kmph";
         _fuelText.text = Mathf.RoundToInt(_carController.Fuel).ToString() + "L";
         _damageText.text = Mathf.RoundToInt(_carController.damagePercentage).ToString() + "% dmg";
+        _moneyText.text = "$" + _money.ToString();
     }
 }
