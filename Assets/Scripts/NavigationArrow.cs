0fea4e03eb0bd9486fbd800dd36ed435de0135d5
WesleyBurgt <wesleyburgt@gmail.com>
2025-04-08 09:59:05 +0200

    Changed NavArrow to arrow. Code keeps initialRotation into consideration

--- Assets/Scripts/NavigationArrow.cs
+++ Assets/Scripts/NavigationArrow.cs
@@ -5,23 +5,29 @@ public class NavigationArrow : MonoBehaviour
     [SerializeField] private DeliverySystem _deliverySystem;
     private MeshRenderer _meshRenderer;
 
+    private Quaternion _initialLocalRotation;
+
     void Start()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
+        _initialLocalRotation = transform.localRotation;
     }
 
     void LateUpdate()
     {
-        Debug.Log(_deliverySystem.CurrentDestinationDeliveryPort);
         if (_deliverySystem.CurrentDestinationDeliveryPort != null)
         {
             _meshRenderer.enabled = true;
             float navSpeed = 1000f;
             float singleStep = navSpeed * Time.deltaTime;
             Vector3 targetDirection = _deliverySystem.CurrentDestinationDeliveryPort.transform.position - transform.position;
-            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-            Debug.DrawRay(transform.position, newDirection, Color.red);
-            transform.rotation = Quaternion.LookRotation(newDirection);
+
+            if (targetDirection != Vector3.zero)
+            {
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+                Quaternion lookRotation = Quaternion.LookRotation(newDirection);
+                transform.rotation = lookRotation * _initialLocalRotation;
+            }
         }
         else
         {
